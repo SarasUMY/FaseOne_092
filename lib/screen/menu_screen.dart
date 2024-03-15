bc94c6b5f007376a32409c32399ee440dffebec5
saraz <bagas.saras.ft21@mail.umy.ac.id>
2024-03-15 21:28:33 +0700

    Add MenuScreen

--- lib/screen/menu_screen.dart
+++ lib/screen/menu_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:ucp1_paml_092/screen/detail_screen.dart';
 import 'package:ucp1_paml_092/widgets/menu_footer.dart';
 import 'package:ucp1_paml_092/widgets/menu_header.dart';
 import 'package:ucp1_paml_092/widgets/menu_widget.dart';
@@ -20,24 +21,44 @@ class MenuScreen extends StatelessWidget {
 
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                MenuHeader(),
-                MenuWidget(
-                  formkey: formkey, 
-                  etMakanan: makanan, 
-                  etMinuman: minuman, 
-                  etDessert: dessert
-                ),
-                
-              ],
-            ),
+          child: Center(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              MenuHeader(),
+              Text("Nama: $nama"),
+              Text("No. Telp: $noTelp"),
+              MenuWidget(
+                  formkey: formkey,
+                  etMakanan: makanan,
+                  etMinuman: minuman,
+                  etDessert: dessert),
+              MenuFooter(onPressedOrder: () {
+                if (formkey.currentState!.validate()) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailScreen(
+                        nama: nama,
+                        noTelp: noTelp,
+                        makanan: makanan.text,
+                        minuman: minuman.text,
+                        dessert: dessert.text,
+                      ),
+                    ),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text("Berhasil Memesan Menu"),
+                    ),
+                  );
+                }
+              })
+            ],
           ),
-        )
-      ),
+        ),
+      )),
     );
   }
 }
